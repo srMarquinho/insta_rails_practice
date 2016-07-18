f538f53d1fd8965143b9b9ae110bb7cf932d4791
srMarquinho <sr.marquinho@gmail.com>
2016-07-18 16:07:10 +0100

    fix heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require 'devise'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -19,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module InstagramChallenge
   class Application < Rails::Application
+    require 'devise'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
